8ee3d1bdf6e068a05c3e75d3c16a1e6368b89772
Michael Ching <mikeching@yahoo.com>
2013-10-19 10:27:21 -1000

    change model to rchis05 (subset of design based on full dataset)

--- src/overlap.R
+++ src/overlap.R
@@ -6,20 +6,20 @@
 library(survey)
 
 # Population level estimates without standard errors
-svytable(~cf46+cf47, rchis)
+svytable(~cf46+cf47, rchis05)
 
 # There are no missing on either cf46 or cf47
 table(is.na(chis$cf47) == is.na(chis$cf46))
 
 # The total of children 
-referred.total <- sum(svytotal(~cf46, na.rm = T, design = rchis))
+referred.total <- sum(svytotal(~cf46, na.rm = T, design = rchis05))
 # sum(svytotal(~cf47, na.rm = T, design = rchis))
 
-a <- svyby(~cf46, by = ~cf47, FUN = svytotal, design = rchis)
+a <- svyby(~cf46, by = ~cf47, FUN = svytotal, design = rchis05)
 a
 a/referred.total
 confint(a)
 confint(a)/referred.total
 
-svymean(~cf46, na.rm = T, rchis)
-svymean(~cf47, na.rm = T, rchis)
+svymean(~cf46, na.rm = T, rchis05)
+svymean(~cf47, na.rm = T, rchis05)
