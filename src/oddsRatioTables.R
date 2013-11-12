162601cad36e53a41112f82eb47e1870b7dc7bf1
Michael Ching <mikeching@yahoo.com>
2013-11-11 21:54:57 -1000

    completed, not quite significant though

--- src/oddsRatioTables.R
+++ src/oddsRatioTables.R
@@ -1,6 +1,11 @@
 # Calculate the odds ratios of PEDS high risk vs no-moderate risk at
 # different ages given significant interaction term
 
+# The result of this is that there is no significant odds ratios at any particular age per this analysis. 
+# Am I doing this wrong? Need to check on how I am doing this analysis...
+
+# Maybe the best next step is to do prediction using ages 0:5 and lbw and non lbw
+
 vcov.either.int <- vcov(mvreg.either.int)
 var.pedsHiRisk <- vcov.either.int[2,2]
 var.pHR.age <- vcov.either.int[13,13]
@@ -17,3 +22,5 @@ log.odds.ratio.lower <- log.odds.ratio + qnorm(0.025) * se.int.coef
 log.odds.ratio.upper <- log.odds.ratio + qnorm(0.975) * se.int.coef
 log.odds.ratio.table <- cbind(log.odds.ratio, log.odds.ratio.lower, log.odds.ratio.upper)
 odds.ratio.table <- exp(log.odds.ratio.table)
+
+plot(odds.ratio.table[, 1])
