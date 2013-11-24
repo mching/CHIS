21757d4467c2cfb6bcef0feb2e10ab136fc1a060
Michael Ching <mikeching@yahoo.com>
2013-11-24 10:24:52 -1000

    initial commits

---
+++ Tables/Table3.R
@@ -0,0 +1,25 @@
+# Table 3 multivariable
+
+stargazer(mvreg.dev, mvreg.dev.int, mvreg.speech, mvreg.speech.int, mvreg.either, mvreg.either.int, 
+          # type = "text",
+          title = "Table: Multivariable Logistic Regression Models",
+          out = "./Tables/Table3.tex",
+          digits = 2,
+          single.row = T,
+          dep.var.labels = c("DS Only", "SLH Only", "Either DS or SLH"), 
+          covariate.labels = c("Male",
+                               "Age",
+                               "Birthweight (kg)",
+                               "Latino",
+                               "Asian",
+                               "African American",
+                               "Pacific Islander/Other",
+                               "Non-Hispanic",
+                               "Below 100% Federal Poverty Level",
+                               "Uninsured in Past Year",                               
+                               "PEDS High Risk * Age",
+                               "PEDS High Risk * Birthweight")
+)
+
+library(knitr)
+pandoc()
