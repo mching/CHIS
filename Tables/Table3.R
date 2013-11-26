bac42477cf18f7b9848d1f6b97e761e19171ed56
Michael Ching <mikeching@yahoo.com>
2013-11-25 20:32:36 -1000

    comment out the labels since the order changed in the model call

--- Tables/Table3.R
+++ Tables/Table3.R
@@ -1,25 +1,22 @@
 # Table 3 multivariable
 
 stargazer(mvreg.dev, mvreg.dev.int, mvreg.speech, mvreg.speech.int, mvreg.either, mvreg.either.int, 
-          # type = "text",
+          type = "text",
           title = "Table: Multivariable Logistic Regression Models",
-          out = "./Tables/Table3.tex",
+          out = "./Tables/Table3.txt",
           digits = 2,
           single.row = T,
-          dep.var.labels = c("DS Only", "SLH Only", "Either DS or SLH"), 
-          covariate.labels = c("Male",
-                               "Age",
-                               "Birthweight (kg)",
-                               "Latino",
-                               "Asian",
-                               "African American",
-                               "Pacific Islander/Other",
-                               "Non-Hispanic",
-                               "Below 100% Federal Poverty Level",
-                               "Uninsured in Past Year",                               
-                               "PEDS High Risk * Age",
-                               "PEDS High Risk * Birthweight")
+          dep.var.labels = c("DS Only", "SLH Only", "Either DS or SLH")
+#           covariate.labels = c("Male",
+#                                "Age",
+#                                "Birthweight (kg)",
+#                                "Latino",
+#                                "Asian",
+#                                "African American",
+#                                "Pacific Islander/Other",
+#                                "Non-Hispanic",
+#                                "Below 100% Federal Poverty Level",
+#                                "Uninsured in Past Year",                               
+#                                "PEDS High Risk * Age",
+#                                "PEDS High Risk * Birthweight")
 )
-
-library(knitr)
-pandoc()
