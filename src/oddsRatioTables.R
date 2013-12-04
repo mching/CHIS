e4ae039e8341a3880f8f22aa59bf8898fea2e01b
Michael Ching <mikeching@yahoo.com>
2013-12-03 20:51:16 -1000

    They are significant now for some reason. Replace dimnames with age and OR titles.

--- src/oddsRatioTables.R
+++ src/oddsRatioTables.R
@@ -1,11 +1,6 @@
 # Calculate the odds ratios of PEDS high risk vs no-moderate risk at
 # different ages given significant interaction term
 
-# The result of this is that there is no significant odds ratios at any particular age per this analysis. 
-# Am I doing this wrong? Need to check on how I am doing this analysis...
-
-# Maybe the best next step is to do prediction using ages 0:5 and lbw and non lbw
-
 vcov.either.int <- vcov(mvreg.either.int)
 var.pedsHiRisk <- vcov.either.int[2,2]
 var.pHR.age <- vcov.either.int[13,13]
@@ -22,5 +17,8 @@ log.odds.ratio.lower <- log.odds.ratio + qnorm(0.025) * se.int.coef
 log.odds.ratio.upper <- log.odds.ratio + qnorm(0.975) * se.int.coef
 log.odds.ratio.table <- cbind(log.odds.ratio, log.odds.ratio.lower, log.odds.ratio.upper)
 odds.ratio.table <- exp(log.odds.ratio.table)
+dimnames(odds.ratio.table)[[1]] <- 0:5
+dimnames(odds.ratio.table)[[2]] <- c("OR", "lower", "upper")
+odds.ratio.table
 
 plot(odds.ratio.table[, 1])
