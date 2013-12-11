4e07f7edae5f32f0a716d7f5e046a4cee87e46f6
Michael Ching <mikeching@yahoo.com>
2013-12-10 22:31:40 -1000

    recenter birthweight to get estimates at median birthweight

--- src/StratAnalysis.R
+++ src/StratAnalysis.R
@@ -2,6 +2,17 @@
 # Stratified analysis by age
 #########
 
+# Recenter birthweight to median
+svyquantile(~brthwk.p.i, design = rchis05, quant = 0.5)
+chis$brthwk.p.i.recenter <- chis$brthwk.p.i - 3.35
+
+# attach to survey design object
+rchis <- svrepdesign(chis[ , -( 212 + ( 1 : 80 ))], repweights = chis[ , ( 212 + ( 1 : 80 ))], weights = chis$rakedw0, combined.weights = TRUE, scale = 1, rscales = rep(1,80), type="other")
+
+# Create subset design object
+rchis05 <- subset(rchis, srage.p < 6)
+
+
 # Create function that will take age and return odds ratios for each outcome
 
 ORAgeStrat <- function(age) {
@@ -13,36 +24,39 @@ ORAgeStrat <- function(age) {
   mvreg.dev <- svyglm(cf46 ~ 
                         male +
                         # srage.p +
-                        brthwk.p.i +
+                        brthwk.p.i.recenter +
                         racehp2p +
                         srh.a.i +
                         belowpovl +
                         unins.ever +
-                        pedsHiRisk
+                        pedsHiRisk +
+                        pedsHiRisk * brthwk.p.i.recenter
                       ,
                       design = rchis.subset, family = quasibinomial)
   
   mvreg.speech <- svyglm(cf47 ~ 
                         male +
                         # srage.p +
-                        brthwk.p.i +
+                        brthwk.p.i.recenter +
                         racehp2p +
                         srh.a.i +
                         belowpovl +
                         unins.ever +
-                        pedsHiRisk
-                      ,
+                        pedsHiRisk +
+                        pedsHiRisk * brthwk.p.i.recenter
+                         ,
                       design = rchis.subset, family = quasibinomial)
   
   mvreg.ref <- svyglm(referred ~ 
                         male +
                         # srage.p +
-                        brthwk.p.i +
+                        brthwk.p.i.recenter +
                         racehp2p +
                         srh.a.i +
                         belowpovl +
                         unins.ever +
-                        pedsHiRisk
+                        pedsHiRisk +
+                        pedsHiRisk * brthwk.p.i.recenter
                       ,
                       design = rchis.subset, family = quasibinomial)
   
@@ -63,13 +77,16 @@ exp(confint(model.list.strat[[5]][[3]]))
 cbind(exp(summary(model.list.strat[[5]][[3]])$coef[, 1]), exp(confint(model.list.strat[[5]][[3]])))
 
 
+ModelNames <- c("DS", "SLH", "DS/SLH")
 
 for(i in 1:6) {
   for(j in 1:3) {
+    print("###################################")
+    print(paste("Age", i, "Model", ModelNames[j]))
     print(cbind(exp(summary(model.list.strat[[i]][[j]])$coef[, 1]), exp(confint(model.list.strat[[i]][[j]]))))
   }
 }
 
-a <- as.list(rep(NA, 3))
-OR.strat.list <- list(a, a, a)
-OR.strat.list <- lapply(OR.strat.list, function(x) lapply(x, function(x) {mean(x)}))
+###########################
+# SLH birthweight interaction stratified analysis
+###########################
