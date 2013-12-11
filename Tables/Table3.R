23d8c03883242342597942a5005fcf08d944b4ba
Michael Ching <mikeching@yahoo.com>
2013-12-10 22:30:34 -1000

    Add table with confidence intervals and also edit the covariate labels

--- Tables/Table3.R
+++ Tables/Table3.R
@@ -4,19 +4,34 @@ stargazer(mvreg.dev, mvreg.dev.int, mvreg.speech, mvreg.speech.int, mvreg.either
           type = "text",
           title = "Table: Multivariable Logistic Regression Models",
           out = "./Tables/Table3.txt",
+          ci = F,
           digits = 2,
-          single.row = T,
-          dep.var.labels = c("DS Only", "SLH Only", "Either DS or SLH")
-#           covariate.labels = c("Male",
-#                                "Age",
-#                                "Birthweight (kg)",
-#                                "Latino",
-#                                "Asian",
-#                                "African American",
-#                                "Pacific Islander/Other",
-#                                "Non-Hispanic",
-#                                "Below 100% Federal Poverty Level",
-#                                "Uninsured in Past Year",                               
-#                                "PEDS High Risk * Age",
-#                                "PEDS High Risk * Birthweight")
+          single.row = F, 
+          star.cutoffs = c(0.05, 0.01, 0.001),
+          dep.var.labels = c("DS", "SLH", "Either DS or SLH"), 
+           covariate.labels = c("Male",
+                                "Age",
+                                "Birthweight (kg)",
+                                "Latino",
+                                "Asian",
+                                "African American",
+                                "Pacific Islander/Other",
+                                "Non-Hispanic",
+                                "Below 100% Federal Poverty Level",
+                                "Uninsured in Past Year", 
+                                "PEDS High Risk",
+                                "PEDS High Risk * Age",
+                                "PEDS High Risk * Birthweight")
+)
+
+# Table with just the interaction term models, confidence intervals
+stargazer(mvreg.dev.int, mvreg.speech.int, mvreg.either.int, 
+          type = "text",
+          title = "Table: Multivariable Logistic Regression Models",
+          out = "./Tables/Table3.txt",
+          ci = T,
+          digits = 2,
+          single.row = F,
+          star.cutoffs = c(0.05, 0.01, 0.001),
+          dep.var.labels = c("DS", "SLH", "Either DS or SLH")
 )
