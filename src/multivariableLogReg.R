99cbab70b8ace0bae36d5fa46dffe3bd89a80574
Michael Ching <mikeching@yahoo.com>
2013-12-11 20:56:50 -1000

    move quadratic terms to its own file

--- src/multivariableLogReg.R
+++ src/multivariableLogReg.R
@@ -123,39 +123,3 @@ regTermTest(mvreg.either.int, test.terms = ~ # pedsHiRisk * srage.p
 regTermTest(mvreg.either.int, test.terms = ~ racehp2p + srh.a.i)
 
 
-# Quadratic
-# Take out some covariates to simplify model. Only interaction
-mvreg.either.0 <- svyglm(referred ~ pedsHiRisk +
-                                   # male +
-                                   srage.p +
-                                   # belowpovl +
-                                   # unins.ever +
-                                   # racehp2p +
-                                   # srh.a.i +
-                                   # brthwk.p.i +
-                                   pedsHiRisk*srage.p 
-                                   # pedsHiRisk*brthwk.p.i +
-                                   # I((srage.p)^2) 
-                                   # I((brthwk.p.i)^2)
-                                 ,
-                                 design = rchis05, family = quasibinomial)
-summary(mvreg.either.0)
-
-# Quadratic age interaction
-mvreg.either.1 <- svyglm(referred ~ pedsHiRisk +
-                           # male +
-                           srage.p +
-                           # belowpovl +
-                           # unins.ever +
-                           # racehp2p +
-                           # srh.a.i +
-                           # brthwk.p.i +
-                           pedsHiRisk*srage.p +
-                         # pedsHiRisk*brthwk.p.i +
-                           I((srage.p)^2)
-                         # I((brthwk.p.i)^2)
-                         ,
-                         design = rchis05, family = quasibinomial)
-summary(mvreg.either.1)
-
-regTermTest(mvreg.either.quadratic, test.terms = ~ pedsHiRisk * srage.p)
